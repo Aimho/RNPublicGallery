8d149ec8bc1c41eba8e7645a3139e14555923568
Aimho <aimho@marblex.net>
2022-04-07 12:11:29 +0900

    feat: 유저 정보 firestore 연동

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -12,8 +12,9 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import SignForm from '../components/SignForm';
 import SignButtons from '../components/SignButtons';
 import {signIn, signUp} from '../lib/auth';
+import {getUser} from '../lib/users';
 
-function SignInScreen({route}) {
+function SignInScreen({navigation, route}) {
   const {isSignUp} = route.params ?? {};
 
   const [loading, setLoading] = useState(false);
@@ -28,18 +29,35 @@ function SignInScreen({route}) {
   };
 
   const onSubmit = async () => {
-    setLoading(true);
     Keyboard.dismiss();
 
-    const {email, password} = form;
+    const {email, password, confirmPassword} = form;
+    if (isSignUp && password !== confirmPassword) {
+      Alert.alert('실패', '비밀번호가 일치하지 않습니다.');
+      return;
+    }
+
+    setLoading(true);
     const info = {email, password};
 
     try {
       const {user} = isSignUp ? await signUp(info) : await signIn(info);
-      console.log(user);
+
+      const profile = await getUser(user.uid);
+      if (!profile) {
+        return navigation.navigate('Welcome', {uid: user.uid});
+      }
+
+      // 구현예정
     } catch (e) {
-      Alert.alert('실패');
-      console.error(e);
+      const messages = {
+        'auth/email-already-in-use': '이미 가입된 이메일입니다.',
+        'auth/wrong-password': '잘못된 비밀번호입니다.',
+        'auth/user-not-found': '존재하지 않는 계정입니다.',
+        'auth/invalid-email': '유효하지 않은 이메일 주소입니다.',
+      };
+      const msg = messages[e.code] || `${isSignUp ? '가입' : '로그인'} 실패`;
+      Alert.alert('실패', msg);
     } finally {
       setLoading(false);
     }
