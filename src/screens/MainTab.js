49b2813d0354081dc3b9ff64b5490ed54914be6d
Aimho <aimho@marblex.net>
2022-04-07 18:28:55 +0900

    feat: 카메라 버튼 및 라이브러리 사진 선택, 카메라 촬영 기능 추가

---
+++ src/screens/MainTab.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import HomeStack from './HomeStack';
+import MyProfileStack from './MyProfileStack';
+
+import CameraButton from '../components/CameraButton';
+
+const Tab = createBottomTabNavigator();
+
+function MainTab() {
+  return (
+    <>
+      <View style={styles.block}>
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+            tabBarShowLabel: false,
+            tabBarActiveTintColor: '#6200ee',
+          }}>
+          <Tab.Screen
+            name="HomeStack"
+            component={HomeStack}
+            options={{
+              tabBarIcon: ({color}) => (
+                <Icon name="home" size={24} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="MyProfileStack"
+            component={MyProfileStack}
+            options={{
+              tabBarIcon: ({color}) => (
+                <Icon name="person" size={24} color={color} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </View>
+
+      <CameraButton />
+    </>
+  );
+}
+
+export default MainTab;
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    zIndex: 0,
+  },
+});
